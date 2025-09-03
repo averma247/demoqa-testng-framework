a404e31c3e88d5d08a1115c9431d530b55b320f7
ajaynagarro <ajay.verma@nagarro.com>
2025-09-04 01:54:48 +0530

    logs added.

--- src/test/java/tests/SampleTest.java
+++ src/test/java/tests/SampleTest.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import driver.DriverManager;
 import reports.ExtentReportManager;
+import utils.LogUtil;
 
 public class SampleTest extends BaseTest {
 
@@ -14,6 +15,9 @@ public class SampleTest extends BaseTest {
 
         ExtentReportManager.getTest().info("Navigated to demoqa.com");
         ExtentReportManager.getTest().info("Page title is: " + title);
+        LogUtil.info("Title of the page: "+title);
         Assert.assertTrue(title.contains("DEMOQA"), "Title does not contain expected text.");
+        LogUtil.debug("Title of the page: "+title);
+
     }
 }
