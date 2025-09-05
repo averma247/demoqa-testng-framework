71f886ec2c5172249a32e65b7771e8523fcc57bb
ajaynagarro <ajay.verma@nagarro.com>
2025-09-06 02:34:38 +0530

    Extent repo config.

--- src/main/java/reports/ExtentReportManager.java
+++ src/main/java/reports/ExtentReportManager.java
@@ -3,6 +3,7 @@ package reports;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReportManager {
     private static ExtentReports extent;
@@ -11,6 +12,18 @@ public class ExtentReportManager {
     public static void initReports() {
         extent = new ExtentReports();
         ExtentSparkReporter spark = new ExtentSparkReporter("Reports/ExtentReport.html");
+        spark.config().setDocumentTitle("Test Report");
+        spark.config().setReportName("Build Report");
+        spark.config().setTheme(Theme.STANDARD);
+        spark.config().setEncoding("utf-8");
+        spark.config().setTimelineEnabled(true);
+        extent.setSystemInfo("OS", System.getProperty("os.name"));
+        extent.setSystemInfo("User", System.getProperty("user.name"));
+        extent.setSystemInfo("Java Version", System.getProperty("java.version"));
+        extent.setSystemInfo("Environment", "QA");
+        extent.setSystemInfo("Browser", "Chrome");
+
+
         extent.attachReporter(spark);
     }
 
--- src/main/java/utils/ScreenshotUtil.java
+++ src/main/java/utils/ScreenshotUtil.java
@@ -11,9 +11,11 @@ import java.io.IOException;
 public class ScreenshotUtil {
     public static String captureScreenshot(String testName) throws IOException {
         File src = ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.FILE);
-        String path = "Screenshots/" + testName + ".png";
+        //String path = "Screenshots/" + testName + ".png";
+        String path = "src/../images/screenshots_"+System.currentTimeMillis()+ testName + ".png";
+
         File finalDest = new File(path);
         FileUtils.copyFile(src, finalDest);
-        return path;
+        return finalDest.getAbsolutePath();
     }
 }
