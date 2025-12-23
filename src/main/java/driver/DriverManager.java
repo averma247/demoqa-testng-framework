9cc5455152e1127ab424cb401383c9164a329868
ajaynagarro <ajay.verma@nagarro.com>
2025-12-23 22:59:02 +0530

    Adding paralle execution on multiple browser.

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -1,7 +1,9 @@
 package driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -22,7 +24,11 @@ public class DriverManager {
             } else {
                 if (browser.equalsIgnoreCase("chrome")) {
                     webDriver = new ChromeDriver();
-                } else {
+                } else if (browser.equalsIgnoreCase("edge")) {
+                    WebDriverManager.edgedriver().setup();
+                    webDriver=new EdgeDriver();
+                }
+                else {
                     webDriver = new FirefoxDriver();
                 }
             }
